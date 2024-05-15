e63448ed46d99968fa36240d899ae48288cdef92
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-05-15 22:50:21 +0200

    Update task-3.js

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+class StringBuilder (initialValue) {
+#value;
+getValue()
+padEnd(str)
+padStart(str)
+padBoth(str)
+}
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
